64f226fb496178978e2e24a3a6301c6bdb2238d4
kanchaan chopraa <kanchaanchopraa@gmail.com>
2025-03-04 00:18:38 +0530

    correction related to optional chaining for API response

--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -16,11 +16,14 @@ const RestaurantMenu = () => {
 
     const fetchMenuData = async () => {
         const data = await fetch(MENU_API_URL+resId);
-        console.log(MENU_API_URL+resId);
         const json = await data.json();
         setResInfo(json);
-        setResMenu(json?.data?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards[1]?.card?.card?.itemCards || json?.data?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards[4]?.card?.card?.itemCards);
-        console.log(json?.data?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards[1]?.card?.card?.itemCards || json?.data?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards[4]?.card?.card?.itemCards);
+
+        const cardArray = json?.data?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards;
+        const menuCard = cardArray.find((card) => card?.card?.card?.itemCards);
+
+        setResMenu(menuCard?.card?.card?.itemCards);
+        console.log(menuCard);
     }
 
     if(resInfo === null || resMenu === null) return (
@@ -46,13 +49,14 @@ const RestaurantMenu = () => {
 
             <div className="cardListWrapper">
                 {resMenu.map((item,i) => {
-                    const {name, imageId, isVeg, description} = item?.card?.info;
+                    const {name, imageId, isVeg, description, price, defaultPrice} = item?.card?.info;
                     return(
                         <>
                             <div className="cardList" key={i}>
                                 <div>
                                     {isVeg ? <div className="isVeg"></div> : <div className="isNonVeg"></div>}
                                     <h3 style={{padding: "6px 0"}}>{name}</h3>
+                                    <h3 style={{padding: "0 0 6px"}}>Rs. {price/100 || defaultPrice/100}</h3>
                                     <p>{description}</p>
                                 </div>
                                 {imageId !== undefined && <img src={MENU_IMG_BASE_PATH+imageId} alt={name} height="100px" width="100px"/>}
