cdc1abbe28f56eb0feec0ea22138c76212ade9ee
kanchaan chopraa <kanchaanchopraa@gmail.com>
2025-05-30 22:14:58 +0530

    updated the folder structure and also added assignment.md files

--- src/exercises/Country.js
+++ src/exercises/Country.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./styles/style1.css";
-import CountryExercise from './components/CountryExercise'
+import CountryExercise from '../components/CountryExercise'
 
 const Country = () => {
     return(
